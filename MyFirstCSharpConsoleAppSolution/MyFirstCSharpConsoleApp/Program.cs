637e0540056983ea0e53bf1d1aebf87ef2eedd3e
naimish1224 <naimish1224@gmail.com>
2021-07-02 12:16:28 -0400

    -u

---
+++ MyFirstCSharpConsoleAppSolution/MyFirstCSharpConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstCSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hw = "Hello, World";
+            var no = 12;
+            var d = 12.32;
+            Console.WriteLine(hw);
+        }
+    }
+}
