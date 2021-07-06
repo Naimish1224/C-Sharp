fae91b1d3671f1b8d5bed949ed61edad4ef64096
naimish1224 <naimish1224@gmail.com>
2021-07-06 14:01:15 -0400

    added bowling game

---
+++ BowlingCollectionsSolution/BowlingCollections/Bowling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingCollections
+{
+    class Bowling
+    {
+        List<int> game = new List<int>();
+
+        public int Bowl()
+        {
+            for (var idx = 1; idx <= 10; idx++)
+            {
+                Console.Write($"Enter score for frame {idx};");
+                var scoreStr = Console.ReadLine();
+                var score = Convert.ToInt32(scoreStr);
+                game.Add(score);
+            }
+            var total = 0;
+            foreach(var score in game)
+            {
+                total += score;
+            }
+            return total;
+        }
+    }
+}
---
+++ BowlingCollectionsSolution/BowlingCollections/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BowlingCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Bowling();
+            Console.WriteLine($"Game score is {game.Bowl()}");
+        }
+    }
+}
