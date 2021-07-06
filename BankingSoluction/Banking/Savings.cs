095eb0b867843442d3b71d4a49c18cc6585ff04b
naimish1224 <naimish1224@gmail.com>
2021-07-06 10:30:06 -0400

    -u

---
+++ BankingSoluction/Banking/Savings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Savings : Account
+    {
+        public decimal InterestRate { get; set; } = 0.12m;
+        private decimal MinBalance { get; set; } = 200.0m;
+
+        public decimal CalculateInterest(int months)
+        {
+            return Balance * (InterestRate / 12) * months;
+        }
+
+    }
+
+
+}
